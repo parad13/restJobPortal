e69bb40525f7d6b013d2ca8fb01ecf688a8a019c
paras waral <56946866+parad13@users.noreply.github.com>
2020-12-23 18:54:51 +0530

    'init'

--- jobs/settings.py
+++ jobs/settings.py
@@ -63,10 +63,10 @@ WSGI_APPLICATION = "jobs.wsgi.application"
 
 DATABASES = {
 
-     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    },
+    #  "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    # },
 
     # 'default': {
     #     'ENGINE': 'django.db.backends.mysql',
@@ -77,14 +77,14 @@ DATABASES = {
     #     'PORT': '3306',
     # },
 
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': config('DB_NAME'),
-    #     'USER': config('DB_USER'),
-    #     'PASSWORD': config('DB_PASSWORD'),
-    #     'HOST': config('DB_HOST'),
-    #     'PORT': '5432',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': '5432',
+    }
 
 }
 
